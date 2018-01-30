fcc398912ef81c31f67994dc30549f39f420c69a
qiru <bjqiru@corp.netease.com>
2018-01-30 20:22:22 +0800

    添加背景

--- 2048/js/main.js
+++ 2048/js/main.js
@@ -5,7 +5,18 @@ export default class Main {
       this.init();
    }
    init(){
-      console.log(canvas)
-      console.log(ctx)
+      let img = new Image();
+      img.src = 'images/bg.png';
+      img.onload = () => {
+         console.log(img.width)
+         for(var i = 0; i< Math.ceil(window.innerWidth / 100);i++){
+            for(var j = 0; j < Math.ceil(window.innerHeight / 100);j++){
+               ctx.drawImage(img, 0, 0, 100, 100, i * 100, j * 100, 100, 100);
+            }
+            
+         }
+         
+      }
+      
    }
 }
