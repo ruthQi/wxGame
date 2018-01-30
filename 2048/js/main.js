f7a1b12d070db0125062fcc8fdbee540497e1399
qiru <bjqiru@corp.netease.com>
2018-01-30 18:25:23 +0800

    添加2048

---
+++ 2048/js/main.js
@@ -0,0 +1,11 @@
+
+let ctx = canvas.getContext('2d');
+export default class Main {
+   constructor(){
+      this.init();
+   }
+   init(){
+      console.log(canvas)
+      console.log(ctx)
+   }
+}
